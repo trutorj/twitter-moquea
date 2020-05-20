006c4877724479dd4d959355fad11d246b1b9fb1
trutorj <trutorj@gmail.com>
2020-05-20 20:53:51 +0200

    api

--- src/statistics.py
+++ src/statistics.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import datetime as dt
+import matplotlib.pyplot as plt
+import io
 
 ##################################################################################
 #                        DO THE STATISTICS                                       #
@@ -13,7 +15,7 @@ def Normalizador(df, column):
 
 # 1. Polen data
 # Import the data
-polen = pd.read_csv('data/SEAIC_Madrid.csv')
+polen = pd.read_csv('~/Documentos/IRONHACK/FINAL/twitter-moquea/data/SEAIC_Madrid.csv')
 
 # Calculate the average
 polen["mean"] = polen[["Subiza", "Infanta_Leonor"]].mean(axis=1)
@@ -31,7 +33,7 @@ for e in polen_years:
 
 #2. Tweet data
 # Import the data
-tweet_series = pd.read_csv('data/tweets_series.csv')
+tweet_series = pd.read_csv('~/Documentos/IRONHACK/FINAL/twitter-moquea/data/tweets_series.csv')
 
 # Convert date to datetime and set it as index
 tweet_series["date"] = pd.to_datetime(tweet_series["date"])
@@ -52,13 +54,36 @@ for e in tweet_years:
 # Join the datasets by date
 joined_list = []
 for i in range(len(polen_years)):
-    joined_list.append(polen_years[i].join(tweet_years[i], how='inner', lsuffix='pol_', rsuffix='twe_'))
+    joined_list.append(polen_years[i].join(tweet_years[i], how='inner', lsuffix='_pol', rsuffix='_twe'))
     
 # Calculate the Pearson correlation for each year
 overall_pearson_r = [] 
 for i in range(len(joined_list)):
-    overall_pearson_r.append(joined_list[i].corr().iloc[3,5])
+    overall_pearson_r.append(round(joined_list[i].corr().iloc[3,5],3))
 
 # Show the Pearson correlation values in a dataframe
 pearson = pd.DataFrame(list(zip(list(range(2017,2021)),overall_pearson_r)), columns=["Year","Pearson"])
 pearson
+
+
+##############################################################
+#                           PLOTS                            #
+##############################################################
+def plot_todos():
+    f,ax=plt.subplots(4,1,figsize=(14,14))
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
+                wspace=None, hspace=0.4)
+    for i,e in enumerate(range(2017,2021)):
+        plt.sca(ax[i])
+        pol=joined_list[i][["normalized_pol"]]
+        twe=joined_list[i][["normalized_twe"]]
+        plt.plot(twe, label = "Number of tweets")
+        plt.plot(pol, label = "Polen grains/$m^3$")
+        plt.legend()
+        plt.title(f'{e}. Overall Pearson r = {overall_pearson_r[i]}')
+
+    # here is the trick save your figure into a bytes object and you can afterwards expose it via flask
+    bytes_image = io.BytesIO()
+    plt.savefig(bytes_image, format='png')
+    bytes_image.seek(0)
+    return bytes_image
